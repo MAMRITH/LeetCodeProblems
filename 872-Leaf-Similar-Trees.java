530dbaddc59d7186d275da1fadf7f91a5b29cadd
MAMRITH U <110555181+MAMRITH@users.noreply.github.com>
2024-09-23 15:48:26 +0530

    [872] [Time Beats: 100.00%] [Memory Beats: 53.87%] - LeetPush

---
+++ 872-Leaf-Similar-Trees.java
@@ -0,0 +1,35 @@
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1=new ArrayList<>();
+        List<Integer>l2=new ArrayList<>();
+        fncn1(root1,l1);
+        fncn2(root2,l2);
+        // compare two lists 
+        // space-->O(n+m)-->where n and m are respective number of leaves in both the trees
+if(l1.equals(l2)==true)
+return true;
+else
+return false;
+    }
+    public static void fncn1(TreeNode root1, List<Integer> l1){
+        if(root1==null)
+        return;
+        if((root1.left==null)&&(root1.right==null)){
+            l1.add(root1.val);
+            return;
+        }
+        fncn1(root1.left,l1);
+            fncn1(root1.right,l1);
+    }
+ public static void fncn2(TreeNode root2, List<Integer> l2){
+     if(root2==null)
+        return;
+        if((root2.left==null)&&(root2.right==null)){
+            l2.add(root2.val);
+            return;
+        }
+          fncn2(root2.left,l2);
+            fncn2(root2.right,l2);
+    }
+}
